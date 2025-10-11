fa7a92ae24316789410b974efd42a4139177a04c
rajugowda1806 <raju@dheecodinglab.com>
2025-10-11 14:27:55 +0530

    Added abstraction to AuthService

---
+++ quickcart/src/main/java/com/donkie/quickcart/user/application/service/AuthService.java
@@ -0,0 +1,15 @@
+package com.donkie.quickcart.user.application.service;
+
+import com.donkie.quickcart.user.application.model.LoginCommand;
+import com.donkie.quickcart.user.application.model.LoginResult;
+
+import java.util.List;
+
+import static com.donkie.quickcart.shared.security.CurrentUser.getCurrentUserRoles;
+
+public interface AuthService {
+
+    public LoginResult.Detail loginUser(LoginCommand.Create create);
+
+    public List<String> getUserRoles();
+}
---
+++ quickcart/src/main/java/com/donkie/quickcart/user/infra/service/AuthServiceImpl.java
@@ -0,0 +1,27 @@
+package com.donkie.quickcart.user.infra.service;
+
+import com.donkie.quickcart.user.application.model.LoginCommand;
+import com.donkie.quickcart.user.application.model.LoginResult;
+import com.donkie.quickcart.user.application.service.AuthService;
+import com.donkie.quickcart.user.infra.integration.keycloak.KeycloakAuthClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.donkie.quickcart.shared.security.CurrentUser.*;
+
+@Service
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final KeycloakAuthClient keycloakAuthClient;
+
+    public LoginResult.Detail loginUser(LoginCommand.Create create) {
+        return keycloakAuthClient.loginUser(create);
+    }
+
+    public List<String> getUserRoles() {
+        return getCurrentUserRoles();
+    }
+}
